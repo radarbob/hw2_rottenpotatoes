5476f3efcc5e750168f8fb9042f4e512e01a4773
radarbob <robert@joneshouse.com>
2013-09-11 11:44:18 -0700

    It's working. Deleted 'puts'es

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,43 +6,25 @@ class MoviesController < ApplicationController
   end
 
   def index
-    puts "**************** start index ********************"
-    puts "params incoming: " + params.inspect
-    
     @all_ratings = Hash[ *Movie.ratings.collect { |v| [ v, 1] }.flatten ] 
     @checked_ratings = params[:ratings].nil? ? @all_ratings : params[:ratings]
-    set_checked_ratings  
-    
-    puts "after set-checked @all-ratings: #{@all_settings}"
+    set_checked_ratings
     
     @checkedRatings = @all_ratings.select {|k,v| v == 1}.keys    # and array for the order clause
-    puts "checkedRatings: #{@checkedRatings.inspect}"
-    
     #sort_by_column  
     @sort_column = params[:sort] || session[:sort]
 
-    puts "@all_ratings: " + @all_ratings.inspect
-    puts "@checkedRatings: " +  @checkedRatings.inspect
-    puts "@sort_column:" + @sort_column.inspect
-    
     @movies = Movie.where(rating: @checkedRatings).order(@sort_column || "")   #the future!
-    
-    puts "************ end index ***************"
   end
   
   def set_checked_ratings 
-    puts "@all_ratings BEFORE setting against params[:ratings]  " + @all_ratings.inspect
-    
     @all_ratings.each do |k, v|
       unless @checked_ratings.has_key? k then @all_ratings[k] = 0 end
     end
     
-    puts "@all_ratings AFTER setting against params[:ratings]  " + @all_ratings.inspect
     if @all_ratings.values.all? {|v| v == 0} then 
       @all_ratings.each do |key,value| @all_ratings[key] = 1 end
     end
-    
-    puts "@all_ratings after values.all?  " + @all_ratings.inspect
   end
 
   def new
